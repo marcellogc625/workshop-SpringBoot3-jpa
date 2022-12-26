5dbc5913c9682f7ff447d648d153e3b9370a95b0
Marcello Galvão <marcellogc25@gmail.com>
2022-12-26 10:45:42 -0300

    Established Many-to-many association with JoinTable.

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -53,6 +53,13 @@ public class TestConfig implements CommandLineRunner {
         Product p5 = new Product(null, "Rails for Dummies",
                 "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
+        //Setting categories for products//
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com",
                 "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com",
@@ -66,9 +73,9 @@ public class TestConfig implements CommandLineRunner {
                 OrderStatus.WAITING_PAYMENT, u1);
 
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
         userRepository.saveAll(Arrays.asList(u1,u2));
+        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 
     }
 }
