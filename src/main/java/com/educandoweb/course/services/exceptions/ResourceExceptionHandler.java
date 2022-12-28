407310bee93b39665f76938db1996abd8c8a5b41
Marcello Galvão <marcellogc25@gmail.com>
2022-12-28 15:38:37 -0300

    Added exception handling feature in 'deleteById' method.

--- src/main/java/com/educandoweb/course/services/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceExceptionHandler.java
@@ -21,5 +21,14 @@ public class ResourceExceptionHandler {
                 request.getRequestURI());
             return ResponseEntity.status(status).body(er);    
          }
-    
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseError(DatabaseException e, HttpServletRequest request){
+        String error = "Database error.";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError er = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+            request.getRequestURI());
+        return ResponseEntity.status(status).body(er);
+    }
+
 }
