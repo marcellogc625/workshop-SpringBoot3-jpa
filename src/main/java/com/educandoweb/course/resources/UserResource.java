0b03b53f6020bac2b7752650f920491d19de4776
Marcello Galvão <marcellogc25@gmail.com>
2022-12-28 11:35:55 -0300

    Added 'UserInsert' function.

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,17 +1,20 @@
 package com.educandoweb.course.resources;
 
-import com.educandoweb.course.services.UserService;
+import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
-
-import java.util.List;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -21,15 +24,23 @@ public class UserResource {
     private UserService userService;
     
     @GetMapping
-    public ResponseEntity <List<User>> findAll(){
+    public ResponseEntity<List<User>> findAll(){
         List <User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity <User> findById(@PathVariable Long id){
+    public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+            buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+    
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,13 +1,13 @@
 package com.educandoweb.course.services;
 
-import com.educandoweb.course.entities.User;
-import com.educandoweb.course.repositories.UserRepository;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
 
 @Service
 public class UserService {
@@ -22,6 +22,10 @@ public class UserService {
     public User findById(Long id){
         Optional <User> obj = userRepository.findById(id);
         return obj.get();
+    }
+
+    public User insert(User obj){
+        return userRepository.save(obj);
     }
 
 }
